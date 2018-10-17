e54342e3244e64bd781512c10f9e66633ac4d5e8
Tom Harmon <thomas.harmon@gmail.com>
2018-10-16 21:56:23 -0500

    Progress through 4d.

--- sakila_hw.sql
+++ sakila_hw.sql
@@ -12,16 +12,50 @@ FROM actor;
 
 #2a. You need to find the ID number, first name, and last name of an actor, 
 #of whom you know only the first name, "Joe." 
-#What is one query would you use to obtain this information?
-
 SELECT actor_id, first_name, last_name 
 FROM actor
 WHERE first_name = "Joe"; 
 
 
 #2b. Find all actors whose last name contain the letters GEN:
-#NOT returning a value? 
 SELECT *
 FROM actor  
-WHERE last_name LIKE "GEN"; 
+WHERE last_name LIKE "%GEN%"; 
+
+#2c Find all actors whose last names contain the letters LI. 
+#This time, order the rows by last name and first name, in that order:
+SELECT *
+FROM actor  
+WHERE last_name LIKE "%LI%"
+ORDER BY last_name, first_name; 
+
+#2d Using IN, display the country_id and country columns of the 
+#following countries: Afghanistan, Bangladesh, and China:
+SELECT country_id, country 
+FROM country 
+WHERE country = "Afghanistan" OR country = "Bangladesh" OR country = "China";
+
+#3a acreate a column in the table actor named description and use the data type BLOB
+ALTER TABLE actor
+ADD description BLOB; 
+
+#3b ALTER TABLE actor 
+ALTER TABLE actor
+DROP description;  
+
+#4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name,
+COUNT(*) AS `num`
+FROM actor
+GROUP BY last_name; 
+
+#4b. List last names of actors and the number of actors who have that last name, 
+#but only for names that are shared by at least two actors
+
+SELECT last_name,
+COUNT(*) AS `num`
+FROM actor
+GROUP BY last_name
+IF num < 1;
+
 
