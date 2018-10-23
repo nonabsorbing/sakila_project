2b282d5ccfeced7cbf4fa11e21228c54f74fdb2f
Tom Harmon <thomas.harmon@gmail.com>
2018-10-23 12:16:20 -0500

    Finished homework.

--- sakila_hw.sql
+++ sakila_hw.sql
@@ -1,79 +1,84 @@
 USE sakila;
 
-#1a. Display the first and last names of all actors from the table actor.
+-- 1a. Display the first and last names of all actors from the table actor.
 SELECT first_name, last_name
 FROM actor;
 
-#1b. Display the first and last name of each actor in a single column in upper 
-#case letters. Name the column Actor Name.
+-- 1b. Display the first and last name of each actor in a single column in upper 
+-- case letters. Name the column Actor Name.
 
 SELECT *, CONCAT(first_name, ' ', last_name) AS Actor_name 
 FROM actor;
 
-#2a. You need to find the ID number, first name, and last name of an actor, 
-#of whom you know only the first name, "Joe." 
+-- 2a. You need to find the ID number, first name, and last name of an actor, 
+-- of whom you know only the first name, "Joe." 
 SELECT actor_id, first_name, last_name 
 FROM actor
 WHERE first_name = "Joe"; 
 
 
-#2b. Find all actors whose last name contain the letters GEN:
+-- 2b. Find all actors whose last name contain the letters GEN:
 SELECT *
 FROM actor  
 WHERE last_name LIKE "%GEN%"; 
 
-#2c Find all actors whose last names contain the letters LI. 
-#This time, order the rows by last name and first name, in that order:
+-- 2c Find all actors whose last names contain the letters LI. 
+-- This time, order the rows by last name and first name, in that order:
 SELECT *
 FROM actor  
 WHERE last_name LIKE "%LI%"
 ORDER BY last_name, first_name; 
 
-#2d Using IN, display the country_id and country columns of the 
-#following countries: Afghanistan, Bangladesh, and China:
+-- 2d Using IN, display the country_id and country columns of the 
+-- following countries: Afghanistan, Bangladesh, and China:
 SELECT country_id, country 
 FROM country 
-WHERE country = "Afghanistan" OR country = "Bangladesh" OR country = "China";
+WHERE country IN ("Afghanistan", "Bangladesh","China");
 
-#3a acreate a column in the table actor named description and use the data type BLOB
+-- 3a acreate a column in the table actor named description and use the data type BLOB
 ALTER TABLE actor
 ADD description BLOB; 
 
-#3b ALTER TABLE actor 
+SELECT * 
+FROM actor;
+
+-- 3b ALTER TABLE actor 
 ALTER TABLE actor
 DROP description;  
 
-#4a. List the last names of actors, as well as how many actors have that last name.
+SELECT * 
+FROM actor;
+
+-- 4a. List the last names of actors, as well as how many actors have that last name.
 SELECT last_name,
 COUNT(*) AS `num`
 FROM actor
 GROUP BY last_name; 
 
-#4b. List last names of actors and the number of actors who have that last name, 
-#but only for names that are shared by at least two actors
-###still trying to get the count filter fixed? 
+-- 4b. List last names of actors and the number of actors who have that last name, 
+-- but only for names that are shared by at least two actors
 SELECT COUNT(last_name) AS "number_with_name"
 	,last_name
 FROM actor
-#WHERE "number_with_name" >=2
-GROUP BY last_name;
+GROUP BY last_name
+HAVING COUNT(last_name) >1
+;
 
-#5a. You cannot locate the schema of the address table. 
-#Which query would you use to re-create it?
+-- 5a. You cannot locate the schema of the address table. 
+-- -- Which query would you use to re-create it?
 SHOW CREATE TABLE address; 
 
-#6a. Use JOIN to display the first and last names, as well 
-#as the address, of each staff member. Use the tables staff and address:
+-- 6a. Use JOIN to display the first and last names, as well 
+-- as the address, of each staff member. Use the tables staff and address:
 
 SELECT staff.first_name, staff.last_name, address.address
 FROM staff
 LEFT JOIN address ON 
 staff.address_id = address.address_id;
 
-#6b. Use JOIN to display the total amount rung up by each staff member 
-#in August of 2005. Use tables staff and payment.
+-- 6b. Use JOIN to display the total amount rung up by each staff member 
+-- in August of 2005. Use tables staff and payment.
 
--- SELECT last_name
 SELECT SUM(amount) AS "August 2005 Sales"
 ,last_name
 FROM staff s
@@ -135,7 +140,6 @@ FROM actor
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names 
 -- and email addresses of all Canadian customers. Use joins to retrieve this information.
 
-
 SELECT cu.first_name, cu.last_name, cu.email
 FROM customer cu
 INNER JOIN address ad ON cu.address_id = ad.address_id
@@ -164,12 +168,92 @@ WHERE film_id IN
     );
     
 -- 7e. Display the most frequently rented movies in descending order.
-
+-- THINK ABOUT THIS SOME MORE? 
 SELECT * FROM rental; -- rental_id, inventory_id
 SELECT * FROM inventory; -- inventory_id, film_id
-SELECT * FROM film; film_id, title 
+SELECT * FROM film; -- film_id, title 
 
-SELECT COUNT(title) 
+SELECT COUNT(title) as "Number of Times Rented" 
+, title
 FROM film f
-LEFT JOIN inventory i ON f.film_id = i.film_id; 
+LEFT JOIN inventory i ON f.film_id = i.film_id
+LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY title
+ORDER BY COUNT(title) DESC; 
 
+
+-- 7f. Write a query to display how much business, in dollars, each store brought in.
+-- There's bad data entries in the payment table at row 424 - a null value for rental_id. 
+SELECT * FROM store; -- store_id
+SELECT * FROM staff; -- store_id, staff_id
+SELECT * FROM rental; -- rental_id?, staff_id
+SELECT * FROM payment; -- amount, rental_id
+
+SELECT SUM(amount) as "Revenue per Store"
+, st.store_id
+FROM payment p
+LEFT JOIN rental r ON p.rental_id = r.rental_id
+LEFT join staff s ON r.staff_id = s.staff_id
+LEFT join store st ON s.store_id = st.store_id
+WHERE s.store_id IS NOT NULL
+GROUP BY s.store_id
+;
+
+-- 7g. Write a query to display for each store its store ID, city, and country.
+SELECT * FROM store; -- store_id, addrees_id
+SELECT * FROM address; -- address_id, city_id
+SELECT * FROM city;  -- city_id, country_id
+SELECT * FROM country; -- country_id, country
+
+SELECT store_id, city, country
+FROM store s
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city ci ON a.city_id = ci.city_id
+INNER JOIN country co ON ci.country_id = co.country_id;
+
+-- 7h. List the top five genres in gross revenue in descending order. 
+-- (Hint: you may need to use the following tables: 
+-- category, film_category, inventory, payment, and rental.)
+
+SELECT * FROM category; -- category_id, name
+SELECT * FROM film_category; -- category_id, film_id
+SELECT * FROM  inventory; -- film_id, inventory_id
+SELECT * FROM rental;  -- inventory_id, rental_id
+SELECT * FROM payment; -- rental_id, amount 
+
+SELECT SUM(amount) AS total_per_genre
+, name
+FROM payment p 
+LEFT JOIN rental r ON p.rental_id = r.rental_id
+LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
+LEFT JOIN film_category fc ON i.film_id = fc.film_id
+LEFT JOIN category c ON fc.category_id = c.category_id
+GROUP BY name
+ORDER BY total_per_genre DESC
+LIMIT 5
+; -- NOT SORTING RIGHT???? 
+
+-- 8a. In your new role as an executive, you would like to have an easy way of viewing 
+-- the Top five genres by gross revenue. Use the solution from the problem above to create a view. 
+-- If you haven't solved 7h, you can substitute another query to create a view.
+
+
+
+CREATE VIEW top_genres AS
+SELECT SUM(amount) as total_per_genre
+, name
+FROM payment p 
+LEFT JOIN rental r ON p.rental_id = r.rental_id
+LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
+LEFT JOIN film_category fc ON i.film_id = fc.film_id
+LEFT JOIN category c ON fc.category_id = c.category_id
+GROUP BY name
+ORDER BY total_per_genre DESC
+LIMIT 5;
+
+-- 8b. How would you display the view that you created in 8a?
+SELECT * FROM top_genres;
+
+-- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+
+DROP VIEW top_genres; 
